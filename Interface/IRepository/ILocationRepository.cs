808f5c26748f928a0b454f9d8b869e9574c01e58
Brogramerr <fagbolade006@gmail.com>
2023-02-12 09:05:41 -0800

    Updated ILocationRepository

--- Interface/IRepository/ILocationRepository.cs
+++ Interface/IRepository/ILocationRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace KANOKO.Interface.IRepository
+﻿using KANOKO.Entity;
+using System.Linq.Expressions;
+
+namespace KANOKO.Interface.IRepository
 {
-    public interface ILocationRepository
+    public interface ILocationRepository: IBaseRepository<Location>
     {
+        Task<Location> GetAsync(int id);
+        Task<IList<Location>> GetAllAsync();
+        Task<Location> GetAsync(Expression<Func<Location, bool>> expression);
+        Task<IList<Location>> GetSelectedAsync(List<int> ids);
+        Task<IList<Location>> GetSelectedAsync(Expression<Func<Location, bool>> expression);
     }
 }
