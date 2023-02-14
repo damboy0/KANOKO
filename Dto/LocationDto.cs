34c9bda76b545a1c353a1ce2cae59c85caa99cbf
Brogramerr <fagbolade006@gmail.com>
2023-02-14 04:10:26 -0800

    updated location Dto

---
+++ Dto/LocationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace KANOKO.Dto
+{
+    public interface LocationDto
+    {
+        public string Address { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+    }
+    public interface LocationRequestModel
+    {
+        public string Address { get; set; }
+    }
+}
